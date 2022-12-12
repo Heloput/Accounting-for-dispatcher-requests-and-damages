1e8ecc757699a648949b2a120a18980a86e8853b
Владислав Молодожонов <molodozhonovladik@mail.ru>
2022-12-12 16:20:16 +0400

    Change title

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import tkinter as tk
 import window as win
+from win32api import GetSystemMetrics
 
 if __name__ == "__main__":
+    resolution = str(GetSystemMetrics(0)) + "x" + str(GetSystemMetrics(1))
     root = tk.Tk()
     print("start")
-    root.geometry("250x150")
+    root.geometry(resolution)
     app = win.Main(root)
     root.mainloop()
